5418bd7f6f75c6714872040270da8442261e7ef2
palloc <palloc.mknet10@gmail.com>
2016-04-20 16:41:45 +0900

    fixed

--- k_clustering.py
+++ k_clustering.py
@@ -1,31 +1,28 @@
 from math import *
-
 p = []
-
 center = [0.0,0.0]
 
 #1-4=1  5-8=2
 file = open("../packet_data_extract/result/tmp")
 line = file.readlines()
 
+#read data
 for i in line:
     try:
         p.append([int(i)])
     except ValueError:
         pass
-
-
+#random clustering
 for i in range(0,len(p)/2):
     p[i].append(1)
     p[i+len(p)/2].append(2)
-
 for i in p:
     try:
         i[1]
     except IndexError:
         i.append(2)
     
-
+#k-means
 for j in range(0,100):
     for i in p:
         if i[1] == 1:
@@ -38,8 +35,6 @@ for j in range(0,100):
             i.append(1)
         else:
             i.append(2)
-
     center = [0.0,0.0]
-
 
 print p
