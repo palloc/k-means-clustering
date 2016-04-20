923bcf376964d31011d06f12343b9098fa4a1691
palloc <palloc.mknet10@gmail.com>
2016-04-20 16:38:23 +0900

    データを使用

--- k_clustering.py
+++ k_clustering.py
@@ -1,15 +1,30 @@
 from math import *
 
-p = [[5],[8],[1],[21],[30],[14],[7],[40]]
+p = []
 
 center = [0.0,0.0]
 
 #1-4=1  5-8=2
+file = open("../packet_data_extract/result/tmp")
+line = file.readlines()
 
-for i in range(len(p)/2):
+for i in line:
+    try:
+        p.append([int(i)])
+    except ValueError:
+        pass
+
+
+for i in range(0,len(p)/2):
     p[i].append(1)
     p[i+len(p)/2].append(2)
-print p
+
+for i in p:
+    try:
+        i[1]
+    except IndexError:
+        i.append(2)
+    
 
 for j in range(0,100):
     for i in p:
@@ -18,15 +33,13 @@ for j in range(0,100):
         elif i[1] == 2:
             center[1] += float(i[0]) / float(len(p)/len(center))
         i.pop()
-    print "center1=%f center2=%f" % (center[0],center[1])
     for i in p:
         if abs(center[0] - i[0]) <= abs(center[1] - i[0]):
             i.append(1)
         else:
             i.append(2)
-    print p
+
     center = [0.0,0.0]
 
 
-
-
+print p
