275317af0c968b10ac822183e1201c82b32b7871
palloc <palloc.mknet10@gmail.com>
2016-04-22 11:47:46 +0900

    don't print p

--- 2D-pkmeans.py
+++ 2D-pkmeans.py
@@ -164,9 +164,7 @@ for i in p:
         i.append(2)
 
 #k-means
-print p
 for j in range(0,100):
-
     count1 = 0
     count2 = 0
     for i in p:
@@ -183,7 +181,6 @@ for j in range(0,100):
     center[0][1] /= float(count1)
     center[1][0] /= float(count2)
     center[1][1] /= float(count2)
-
     for i in p:
         if abs(sqrt((center[0][0]-i[0])**2 + (center[0][1]-i[1])**2)) <= abs(sqrt((center[1][0]-i[0])**2 + (center[1][1]-i[1])**2)):
             i.append(1)
@@ -204,7 +201,8 @@ for i in p:
         x2.append(i[0])
         y2.append(i[1])
 
-fig=plt.figure()
+#show figure
+fig = plt.figure()
 subplot = fig.add_subplot(1,1,1)
 subplot.scatter(x1,y1,marker='x',color="red")
 subplot.scatter(x2,y2,marker='o',color="blue")
@@ -212,5 +210,4 @@ subplot.set_xlim(0,65535)
 subplot.set_ylim(0,65535)
 fig.show()
 raw_input()
-
 
